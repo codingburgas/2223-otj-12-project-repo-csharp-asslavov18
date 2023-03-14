39f8e87a62a17504d15c14d88ee227ad7b0d53d7
asslavov18 <asslavov18@codingburgas.bg>
2023-03-14 15:01:26 +0200

    Add navigation menu

---
+++ NutriApp/NutriApp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace NutriApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelMenu = new Panel();
+            panelLogo = new Panel();
+            btnProducts = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panelMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(button5);
+            panelMenu.Controls.Add(button4);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Controls.Add(btnProducts);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 450);
+            panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
+            panelLogo.Paint += panelLogo_Paint;
+            // 
+            // btnProducts
+            // 
+            btnProducts.Dock = DockStyle.Top;
+            btnProducts.FlatAppearance.BorderSize = 0;
+            btnProducts.FlatStyle = FlatStyle.Flat;
+            btnProducts.ForeColor = Color.Gainsboro;
+            btnProducts.Image = Properties.Resources.final_cart_3;
+            btnProducts.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProducts.Location = new Point(0, 80);
+            btnProducts.Name = "btnProducts";
+            btnProducts.Padding = new Padding(12, 0, 0, 0);
+            btnProducts.Size = new Size(220, 60);
+            btnProducts.TabIndex = 1;
+            btnProducts.Text = "Products";
+            btnProducts.TextAlign = ContentAlignment.MiddleLeft;
+            btnProducts.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Gainsboro;
+            button1.Image = Properties.Resources.final_cart_3;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 140);
+            button1.Name = "button1";
+            button1.Padding = new Padding(12, 0, 0, 0);
+            button1.Size = new Size(220, 60);
+            button1.TabIndex = 2;
+            button1.Text = "Products";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.Gainsboro;
+            button2.Image = Properties.Resources.final_cart_3;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 200);
+            button2.Name = "button2";
+            button2.Padding = new Padding(12, 0, 0, 0);
+            button2.Size = new Size(220, 60);
+            button2.TabIndex = 3;
+            button2.Text = "Products";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.Gainsboro;
+            button3.Image = Properties.Resources.final_cart_3;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 260);
+            button3.Name = "button3";
+            button3.Padding = new Padding(12, 0, 0, 0);
+            button3.Size = new Size(220, 60);
+            button3.TabIndex = 4;
+            button3.Text = "Products";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Top;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.Gainsboro;
+            button4.Image = Properties.Resources.final_cart_3;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(0, 320);
+            button4.Name = "button4";
+            button4.Padding = new Padding(12, 0, 0, 0);
+            button4.Size = new Size(220, 60);
+            button4.TabIndex = 5;
+            button4.Text = "Products";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.Gainsboro;
+            button5.Image = Properties.Resources.final_cart_3;
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(0, 380);
+            button5.Name = "button5";
+            button5.Padding = new Padding(12, 0, 0, 0);
+            button5.Size = new Size(220, 60);
+            button5.TabIndex = 6;
+            button5.Text = "Products";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelMenu);
+            Name = "Form1";
+            Text = " ";
+            panelMenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Panel panelLogo;
+        private Button btnProducts;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ NutriApp/NutriApp/Form1.cs
@@ -0,0 +1,20 @@
+namespace NutriApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelLogo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
