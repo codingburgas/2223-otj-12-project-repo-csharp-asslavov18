f62f10de6ea9c75d9d61e1222647a36053346cd7
asslavov18 <asslavov18@codingburgas.bg>
2023-03-15 11:15:50 +0200

    Add ThemeColor class

---
+++ NutriApp/NutriApp/FormMainMenu.cs
@@ -0,0 +1,13 @@
+namespace NutriApp
+{
+    public partial class FormMainMenu : Form
+    {
+        //Fields
+        private Button currentButton;
+        public FormMainMenu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
